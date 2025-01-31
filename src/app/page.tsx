a96885b09ea1da1ce587a9c1178970ba2ab5a859
Hendra Gunawan <hendragunz@codecampz.com>
2025-01-31 14:59:07 +0700

    work on profile component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import Image from "next/image"
+import Profile from "@/components/profile"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-6">
+      <Profile />
+
+      <div className="rounded-2xl bg-white p-6 shadow dark:bg-black dark:shadow-dark lg:col-span-2 lg:p-10">
+        <h1>Site Under Construction</h1>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,10 +16,8 @@ export default function Home() {
           height={38}
           priority
         />
+      </div>
+    </main>
 
-        <h1>Site Under Construction</h1>
-      </main>
-
-    </div>
-  );
+  )
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link"
+import Script from "next/script"
 
 export default function Footer() {
   return (
-    <footer className="text-center">
-      <p className="text-sm dark:text-light/70">
-        @ HendraFolio 2023, Design By
-        <Link
-          href="#"
-          className="inline-block border-b border-b-transparent text-primary transition hover:border-b-primary hover:text-blue-600">
-          &nbsp;Hendra Gunawan
-        </Link>
-      </p>
-    </footer>
+    <>
+      <footer className="text-center">
+        <p className="text-sm dark:text-light/70">
+          @ HendraFolio 2023, Design By
+          <Link
+            href="#"
+            className="inline-block border-b border-b-transparent text-primary transition hover:border-b-primary hover:text-blue-600">
+            &nbsp;Hendra Gunawan
+          </Link>
+        </p>
+      </footer>
+    </>
   )
 }
