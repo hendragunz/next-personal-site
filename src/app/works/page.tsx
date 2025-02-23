9761c79b7c235d180e47faee20ff8e6890feed5b
Hendra Gunawan <hendragunz@codecampz.com>
2025-02-23 09:44:19 +0700

    update work experiences

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,5 +1,195 @@
+import Link from "next/link"
+
 export default function Works() {
   return (
-    <h1>This page under constraction</h1>
+    <>
+      <h2 className="text-3xl font-semibold leading-tight text-dark dark:text-light lg:text-[40px] lg:leading-tight">Work Experience</h2>
+
+
+      <ol className="mt-4 relative border-s border-gray-200 dark:border-gray-700">
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Jam 2019 - Jan 2025 . 6 yrs 1mo</time>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Ruby on Rails Developer</h3>
+
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            At the beginning, I joined with this company as Freelancer because got referred from my comrade (Rails programmer too) and we build everything together for GigWage since scratch.
+            Then after more than 1 year, I switch to work as full-time (remote) for GigWage company and keep continuing to develop with more features and so on. With the skillful and great team in GigWage, we successfully build the great Web Application for Payment solution for Businesses to their Gig Worker.
+          </p>
+
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            My responsibilities for this role:
+          </p>
+
+          <ol className="list-disc mb-4 text-base font-normal text-gray-500 dark:text-gray-400 ms-4">
+            <li>Senior Ruby and Rails programmer</li>
+            <li>Build the RESTFull API System with full documentation</li>
+            <li>Build the backoffice for internal use</li>
+            <li>Make integration to our partner, BaaS to process the payments</li>
+            <li>Research and Development with 1099 Integration to IRS (NEC, K, and MISC)</li>
+            <li>Support, Maintenance and Development for the system</li>
+          </ol>
+
+          <Link
+            href="https://www.gigwage.com/"
+            target="_blank"
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+            Learn more
+            <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+          </Link>
+        </li>
+
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">April 2013 - Dec 2022 . 8 yrs</time>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Ruby on Rails Developer (part-time)</h3>
+
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            As Senior Programmer on this team, my responsibilities are maintenance the system and keep the system up to date with latest Rails version. And also adding more features time by time as requested with best practices way in Ruby on Rails
+          </p>
+
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            And also manage small team with 2 developers (RoR) who help working on new features and RSpec and 1 data entry person for administration and testing.
+          </p>
+
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            My responsibilities for this role:
+          </p>
+
+          <ol className="list-disc mb-4 text-base font-normal text-gray-500 dark:text-gray-400 ms-4">
+            <li>Senior Ruby on Rails programmer</li>
+            <li>Research and Development support this web application</li>
+            <li>Maintenance the application and keep the development progress</li>
+            <li>Handle the team so they can help to develop more features for ReadyFlightAs</li>
+          </ol>
+
+          <Link
+            href="https://production.readyflight.com"
+            target="_blank"
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+            Learn more
+            <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+          </Link>
+        </li>
+
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">April 2013 - June 2019 . 6 yrs 3mo</time>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Codecampz</h3>
+
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            During this time, I begin my career as full-time independent freelance. And day by day, with many clients come for asking my service, I have an idea to make own own team. This is why Codecampz appeared and we have strong team with specific on Ruby on Rails for our stack to build Web Development
+          </p>
+
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            My responsibilities for this role:
+          </p>
+
+          <ol className="list-disc mb-4 text-base font-normal text-gray-500 dark:text-gray-400 ms-4">
+            <li>Team leader, manage the team with the given tasks / projects so it can be finished as deadline</li>
+            <li>Senior Ruby on Rails programmer</li>
+            <li>Training and support for my team to have strongest skill in Ruby/Rails Web Development</li>
+            <li>Research about new technologies (Web & Mobile Development, Server securities)</li>
+            <li>Consulting about web and mobile development</li>
+            <li>Give a service to build Web and RESTFull API System for all of my clients</li>
+          </ol>
+        </li>
+
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 2011 - April 2013 . 1 yrs 7mo</time>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">PT Domikado</h3>
+
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            During this time, I begin my career as full-time independent freelance. And day by day, with many clients come for asking my service, I have an idea to make own own team. This is why Codecampz appeared and we have strong team with specific on Ruby on Rails for our stack to build Web Development
+          </p>
+
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            My responsibilities for this role:
+          </p>
+
+          <ol className="list-disc mb-4 text-base font-normal text-gray-500 dark:text-gray-400 ms-4">
+            <li>Ruby on Rails developer</li>
+            <li>HTML / CSS</li>
+            <li>Research, to get the best how to build good Rails application structure</li>
+            <li>Learning new technologies, API System, RESTFul API</li>
+            <li>Maintenance and bugs fix supportMy</li>
+          </ol>
+
+          <Link
+            href="https://www.linkedin.com/company/pt.-domikado/"
+            target="_blank"
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+            Learn more
+            <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+          </Link>
+        </li>
+
+
+        <li className="mb-10 ms-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March 2010 - Sept 2011 . 1 yrs 3mo</time>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Ruby on Rails Developer</h3>
+
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            I begin my career here as Ruby on Rails programmer.
+            Switched from PHP to Ruby on Rails programmer and here I worked as team within 5-6 people.
+          </p>
+
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            My responsibilities for this role:
+          </p>
+
+          <ol className="list-disc mb-4 text-base font-normal text-gray-500 dark:text-gray-400 ms-4">
+            <li>Learn new technology, Ruby on Rails</li>
+            <li>support the team to deliver the product as per Client requested</li>
+            <li>working on Buddypilot project</li>
+            <li>Learn Javascript / jQuery technology</li>
+            <li>Learn how to deployment Rails application to Heroku / AWS / VPS</li>
+            <li>and many more</li>
+          </ol>
+
+
+          <Link
+            href="https://wgs.co.id"
+            target="_blank"
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+            Learn more
+            <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+          </Link>
+        </li>
+      </ol>
+
+
+    </>
+
   )
 }
